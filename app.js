6caec618fb21b5e3b7e0b3c200de3c0f6c7aa2d0
priyag88 <priyaprograms@gmail.com>
2021-03-11 23:53:24 -0800

    Add user route to entry file

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const users = require('./routes/api/users');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -11,6 +13,10 @@ mongoose
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
