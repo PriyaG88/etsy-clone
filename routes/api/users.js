f2ecf6fcdbfb72c48d48b1b29810d7d96049a624
priyag88 <priyaprograms@gmail.com>
2021-03-12 13:29:14 -0800

    Set up log in route

--- routes/api/users.js
+++ routes/api/users.js
@@ -32,5 +32,26 @@ router.post('/register', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'This user does not exist' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(404).json({ password: 'Incorrect password' });
+                    }
+                });
+        });
+
+});
+
 module.exports = router;
 
