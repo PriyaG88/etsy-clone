312412fcfc88bbebb6b9bc83fd8ec27e1111b2e3
priyag88 <priyaprograms@gmail.com>
2021-03-12 13:59:57 -0800

    Import passport and add middleware

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
