c3afb66cb24169004c85993e044fb6d391a34bea
priyag88 <priyaprograms@gmail.com>
2021-03-12 13:40:33 -0800

    Add web token on user log in

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 router.get('/test', (req,res) => {
     res.json({ msg: 'This is the usersroute' });
@@ -44,12 +46,23 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if (isMatch) {
-                        res.json({ msg: 'Success' });
+                        const payload = { id: user.id, handle: user.handle };
+
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
                     } else {
                         return res.status(404).json({ password: 'Incorrect password' });
                     }
                 });
-        });
+        }); 
 
 });
 
